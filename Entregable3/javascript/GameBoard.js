6eb021a399bd97d2072956a964b00e239bc0467a
Casado Horacio <hcasado@santandertecnologia.com.ar>
2022-10-22 08:46:03 -0300

    test refactor and prettier

--- Entregable3/javascript/GameBoard.js
+++ Entregable3/javascript/GameBoard.js
@@ -1,134 +1,149 @@
-class GameBoard{
+class GameBoard {
+  tokensPerLine
+  board
+  nextPlayer = 1
+  spacesLeft
+  lastChosenColumn
+  rowsAmount
+  columnsAmount
 
-    tokensPerLine
-    board
-    nextPlayer = 1
-    spacesLeft
-    lastChosenColumn
-    rowsAmount
-    columnsAmount
+  //los puntos cardinales representan una dirección y los valores de X e Y se utilizan para mover los indices en la dirección indicada
+  // movimientos en x: -1 arriba, 1 abajo, y 0 fijo
+  // movimientos en y: -1 izquierda, 1 derecha , y 0 fijo
+  directionsMovements = {
+    N: { x: -1, y: 0 },
+    S: { x: 1, y: 0 },
+    E: { x: 0, y: 1 },
+    W: { x: 0, y: -1 },
+    NE: { x: -1, y: 1 },
+    SE: { x: 1, y: 1 },
+    NW: { x: -1, y: -1 },
+    SW: { x: 1, y: -1 },
+  }
 
-    //los puntos cardinales representan una dirección y los valores de X e Y se utilizan para mover los indices en la dirección indicada
-    // movimientos en x: -1 arriba, 1 abajo, y 0 fijo
-    // movimientos en y: -1 izquierda, 1 derecha , y 0 fijo
-    directionsMovements = {
-        "N": {x: -1, y: 0},
-        "S": {x: 1, y: 0},
-        "E": {x: 0, y: 1},
-        "W": {x: 0, y: -1},
-        "NE": {x: -1, y: 1},
-        "SE": {x: 1, y: 1},
-        "NW": {x: -1, y: -1},
-        "SW": {x: 1, y: -1}
+  //lineas creadas por dos segmentos indicados con puntos cardinales, estas lineas se representan como funciones matemáticas
+  lines = {
+    "x = 0": { s1: "N", s2: "S" },
+    "y = 0": { s1: "E", s2: "W" },
+    "y = x": { s1: "NE", s2: "SW" },
+    "y = -1": { s1: "NW", s2: "SE" },
+  }
+
+  constructor(tokensPerLine) {
+    this.tokensPerLine = tokensPerLine
+
+    //se agrega una fila y una columna por cada extra ficha que se quiera agregar después del 4 en linea para hacer el 5, 6 o 7 en linea
+    const extraRowsAndColumns = this.tokensPerLine % 4
+    this.rowsAmount = 6 + extraRowsAndColumns
+    this.columnsAmount = 7 + extraRowsAndColumns
+
+    this.board = Array(this.rowsAmount)
+      .fill()
+      .map(() => Array(this.columnsAmount).fill())
+    this.spacesLeft = this.rowsAmount * this.columnsAmount
+  }
+
+  //#######################################
+  //########## PUBLIC METHODS!!! ##########
+  //return true si la ficha se pudo agregar
+  dropToken(chosenColumn) {
+    //le resto 1 xq los arreglo empiezan en cero xD
+    chosenColumn--
+
+    const row = this.findRowForNewToken(chosenColumn)
+    if (row === -1) {
+      console.log(`the column ${chosenColumn} is full`)
+      return false
     }
 
-    //lineas creadas por dos segmentes según las direcciones dadas, las lineas se representan como funciones matemáticas
-    lines = {
-        "x = 0": {s1: "N",s2: "S"},
-        "y = 0": {s1: "E", s2: "W"},
-        "y = x": {s1: "NE", s2: "SW"},
-        "y = -1": {s1: "NW", s2: "SE"}
+    console.log(
+      `dropping token in column:${chosenColumn}  row:${row} for player:${this.nextPlayer}`
+    )
+    this.board[row][chosenColumn] = this.nextPlayer
+
+    this.nextPlayer = (this.nextPlayer % 2) + 1
+    this.lastChosenColumn = chosenColumn
+    this.spacesLeft--
+    return true
+  }
+
+  isGameOver() {
+    return this.spacesLeft === 0 || this.haslastPlayerWon()
+  }
+
+  getNextPlayer() {
+    return this.nextPlayer
+  }
+
+  getLastPlayer() {
+    return (this.nextPlayer % 2) + 1
+  }
+
+  haslastPlayerWon() {
+    console.log("checking if last player has won...")
+    console.log(
+      `last token played column:${this.lastChosenColumn} row:${
+        this.findRowForNewToken(this.lastChosenColumn) + 1
+      }`
+    )
+
+    for (const line of Object.keys(this.lines)) {
+      if (this.getAmoutOfEqualTokensInLine(line) >= this.tokensPerLine)
+        return true
     }
+    return false
+  }
 
-    constructor(tokensPerLine){
-        this.tokensPerLine = tokensPerLine
+  //#######################################
+  //########## PRIVATE METHODS!!! ##########
 
-        //se agrega una fila y una columna por cada extra ficha que se quiera agregar después del 4 en linea para hacer el 5, 6 o 7 en linea
-        const extraRowsAndColumns = this.tokensPerLine % 4
-        this.rowsAmount = 6 + extraRowsAndColumns
-        this.columnsAmount = 7 + extraRowsAndColumns
+  getAmoutOfEqualTokensInLine(lineFunction) {
+    //retorno la suma de los dos segmentos que forman la linea más la ficha que se jugó
+    const line = this.lines[lineFunction]
+    const amount =
+      1 +
+      this.getAmountOfEqualTokensOfDirection(line.s1) +
+      this.getAmountOfEqualTokensOfDirection(line.s2)
 
-        this.board = Array(this.rowsAmount).fill().map(()=>Array(this.columnsAmount).fill())
-        this.spacesLeft = this.rowsAmount * this.columnsAmount
+    console.log(`line ${lineFunction} has an amount of ${amount} equal tokens`)
+    return amount
+  }
+
+  getAmountOfEqualTokensOfDirection(cardinalPoint) {
+    let { x, y } = this.directionsMovements[cardinalPoint]
+
+    //según la dirección que se indique, muevo los indices para posicionarme en la siguiente ficha
+    let c = this.lastChosenColumn + y
+    let r = this.findRowForNewToken(this.lastChosenColumn) + 1 + x
+
+    let count = 0
+
+    //mientras ningún indice se vaya de rango cuento las fichas iguales a la del jugador
+    while (r < this.rowsAmount && r >= 0 && c <= this.columnsAmount && c >= 0) {
+      if (this.board[r][c] === this.getLastPlayer()) {
+        count++
+        r += x
+        c += y
+      } else break //si encuentro una ficha distinta a la del jugador, no tiene sentido seguir contando
     }
+    return count
+  }
 
-    //#######################################
-    //########## PUBLIC METHODS!!! ##########
-    dropToken(chosenColumn){
-        chosenColumn-- //le resto 1 xq los arreglo empiezan en cero xD
-        this.lastChosenColumn = chosenColumn
-
-        const row = this.findRowForNewToken()
-
-        console.log(`token dropped in column:${chosenColumn}  row:${row} for player:${this.nextPlayer}`)
-        this.board[row][chosenColumn] = this.nextPlayer
-        this.nextPlayer = this.nextPlayer % 2 + 1
-        this.spacesLeft--
+  findRowForNewToken(column) {
+    for (let r = this.rowsAmount - 1; r >= 0; r--) {
+      if (this.board[r][column] == undefined) return r
     }
+    //si la columna elegida está llena devolvemos un valor discernible
+    return -1
+  }
 
-    isGameOver(){
-        return this.spacesLeft === 0 || this.haslastPlayerWon()
-    }
+  getNextPlayer() {
+    return this.nextPlayer
+  }
 
-    getNextPlayer(){
-        return this.nextPlayer
-    }
-
-    getLastPlayer(){
-        return this.nextPlayer % 2 + 1
-    }
-
-    haslastPlayerWon(){
-        console.log("checking if last player has won...")
-        console.log(`last token played column:${this.lastChosenColumn} row:${this.findRowForNewToken() + 1}`)
-
-        for (const line of Object.keys(this.lines)) {
-            if(this.getAmoutOfEqualTokensInLine(line) >= this.tokensPerLine)
-                return true
-        }
-        return false
-    }
-
-    //#######################################
-    //########## PRIVATE METHODS!!! ##########
-
-    getAmoutOfEqualTokensInLine(lineFunction){
-        //retorno la suma de los dos segmentos que forman la linea más la ficha que se jugó
-        const line = this.lines[lineFunction]
-        const amount = 1   
-        + this.getAmountOfEqualTokensOfDirection(line.s1) 
-        + this.getAmountOfEqualTokensOfDirection(line.s2)
-        
-        console.log(`line ${lineFunction} has an amount of ${amount} equal tokens`)
-        return amount
-    }
-
-    getAmountOfEqualTokensOfDirection(cardinalPoint){
-        let {x, y} = this.directionsMovements[cardinalPoint]
-
-        //según la dirección que se indique, muevo los indices para posicionarme en la siguiente ficha
-        let c = this.lastChosenColumn + y
-        let r = this.findRowForNewToken() + 1 + x
-
-        let count = 0;
-
-        //mientras ningún indice se vaya de rango cuento las fichas iguales a la del jugador
-        while(r < this.rowsAmount && r >= 0 && c <=this.columnsAmount && c >= 0){
-            if(this.board[r][c] === this.getLastPlayer()){
-                count++
-                r += x
-                c += y
-            }
-            else break //si encuentro una ficha distinta a la del jugador, no tiene sentido seguir contando
-        }
-        return count
-    }
-
-    findRowForNewToken(){
-        for(let r = this.rowsAmount -1; r >= 0; r--){
-            if(this.board[r][this.lastChosenColumn] == undefined ) return r
-        }
-        //si la columna elegida está llena devolvemos un valor discernible
-        return -1
-    }
-
-    getNextPlayer(){
-        return this.nextPlayer
-    }
-
-    getLastPlayer(){
-        return this.nextPlayer % 2 + 1
-    }
+  getLastPlayer() {
+    return (this.nextPlayer % 2) + 1
+  }
 }
 
 module.exports = { GameBoard }
--- Entregable3/tests/GameBoard.test.js
+++ Entregable3/tests/GameBoard.test.js
@@ -1,97 +1,110 @@
-const { GameBoard } = require('./../javascript/GameBoard');
+const { GameBoard } = require("./../javascript/GameBoard")
 
 let gameBoard
-describe('test board construction', ()=>{
-    
-    it('should be a 6 rows board', () => {
-        gameBoard = new GameBoard(4)
-        const rows = gameBoard.board.length
-        expect(rows).toBe(6)
-    });
-
-    it('should be a 7 columns board', () => {
-        gameBoard = new GameBoard(4)
-        const columns = gameBoard.board[0].length
-        expect(columns).toBe(7)
-    });
-    
-    it('should be a 7 rows board', () => {
-        gameBoard = new GameBoard(5)
-        const rows = gameBoard.board.length
-        expect(rows).toBe(7)
-    });
-
-    it('should be a 8 columns board', () => {
-        gameBoard = new GameBoard(5)
-        const columns = gameBoard.board[0].length
-        expect(columns).toBe(8)
-    });
-    
-    it('should be a 8 rows board', () => {
-        gameBoard = new GameBoard(6)
-        const rows = gameBoard.board.length
-        expect(rows).toBe(8)
-    });
-      
-    it('should be a 9 columns board', () => {
-        gameBoard = new GameBoard(6)
-        const columns = gameBoard.board[0].length
-        expect(columns).toBe(9)
-    });
-})
-
-describe('test token-dropping', ()=>{
+describe("test board construction", () => {
+  it("should be a 6 rows board", () => {
     gameBoard = new GameBoard(4)
+    const rows = gameBoard.board.length
+    expect(rows).toBe(6)
+  })
 
-    test('if the board has a token in the first column after dropping a token', () => {
-        gameBoard = new GameBoard(4)
-        gameBoard.dropToken(1)
-        expect(gameBoard.board[5][0]).toBe(gameBoard.getLastPlayer())
-    });
+  it("should be a 7 columns board", () => {
+    gameBoard = new GameBoard(4)
+    const columns = gameBoard.board[0].length
+    expect(columns).toBe(7)
+  })
 
-    test('if the board has a token of the second player after two token-dropping',()=>{
-        gameBoard = new GameBoard(4)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(2)
-        expect(gameBoard.board[5][1]).toBe(2)
-    })
+  it("should be a 7 rows board", () => {
+    gameBoard = new GameBoard(5)
+    const rows = gameBoard.board.length
+    expect(rows).toBe(7)
+  })
 
+  it("should be a 8 columns board", () => {
+    gameBoard = new GameBoard(5)
+    const columns = gameBoard.board[0].length
+    expect(columns).toBe(8)
+  })
+
+  it("should be a 8 rows board", () => {
+    gameBoard = new GameBoard(6)
+    const rows = gameBoard.board.length
+    expect(rows).toBe(8)
+  })
+
+  it("should be a 9 columns board", () => {
+    gameBoard = new GameBoard(6)
+    const columns = gameBoard.board[0].length
+    expect(columns).toBe(9)
+  })
 })
 
-describe('test winning condition', ()=>{
-    it('should return false when no one has played', ()=>{
-        gameBoard = new GameBoard(4)
-        expect(gameBoard.haslastPlayerWon()).toBe(false)
-    })
+describe("test token-dropping", () => {
+  gameBoard = new GameBoard(4)
 
-    it('should return true when a player has won', ()=>{
-        gameBoard = new GameBoard(4)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(2)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(2)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(2)
-        gameBoard.dropToken(1)
-        expect(gameBoard.haslastPlayerWon()).toBe(true)
-    })
+  test("if the board has a token in the first column after dropping a token", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(1)
+    expect(gameBoard.board[5][0]).toBe(gameBoard.getLastPlayer())
+  })
+
+  test("if the board has a token of the second player after two token-dropping", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(2)
+    expect(gameBoard.board[5][1]).toBe(2)
+  })
+
+  it("should return false when chosen column is full", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+  })
 })
 
-describe('test row search of a token dropped', ()=>{
-    test('if the row of the first token dropped is the first one ',()=>{
-        gameBoard = new GameBoard(4)
-        gameBoard.dropToken(6)
-        expect(gameBoard.findRowForNewToken() + 1).toBe(5)
-    })
+describe("test winning condition", () => {
+  it("should return false when no one has played", () => {
+    gameBoard = new GameBoard(4)
+    expect(gameBoard.haslastPlayerWon()).toBe(false)
+  })
 
-    test('if the row of the sixth token dropped in a column is the last one in a 6x7 board',()=>{
-        gameBoard = new GameBoard(4)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(1)
-        gameBoard.dropToken(1)
-        expect(gameBoard.findRowForNewToken() + 1).toBe(0)
-    })
+  it("should return true when a player has won", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(2)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(2)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(2)
+    gameBoard.dropToken(1)
+    expect(gameBoard.haslastPlayerWon()).toBe(true)
+  })
+})
+
+describe("test row search of a token dropped", () => {
+  test("if the row of the first token dropped is the first one ", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(6)
+
+    //le resto 1 a la columna xq internamente la 6 es el 5 xq los arreglos empiezan en 0
+    expect(gameBoard.board[5][6 - 1]).toBe(gameBoard.getLastPlayer())
+  })
+
+  test("if the row of the sixth token dropped in a column is the last one in a 6x7 board", () => {
+    gameBoard = new GameBoard(4)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+    gameBoard.dropToken(1)
+
+    //cuando una columna está llena findForwForNewToken devuelve -1
+    expect(gameBoard.board[0][0]).toBe(gameBoard.getLastPlayer())
+  })
 })
