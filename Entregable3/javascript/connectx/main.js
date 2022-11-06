d719e479275a657b0adb6d5b2e4504ef3ad0a021
EmaBrites <emabrio@gmail.com>
2022-11-06 20:16:05 -0300

    selccion de modo de juego y ficha

--- Entregable3/javascript/connectx/main.js
+++ Entregable3/javascript/connectx/main.js
@@ -1,21 +1,48 @@
 import ConnectX from "./ConnectX.js"
 
 const gameConfiguration = {
-  tokensPerLine: 7,
+  tokensPerLine: 0,
   cellStyle: "round",
-  tokenColorPlayer1: "red",
-  tokenColorPlayer2: "blue"
+  tokenColorPlayer1: "",
+  tokenColorPlayer2: ""
 }
+let playButton = document.querySelector(".play-game-button")
+let modeButtons = document.querySelectorAll(".game-mode-button")
+let tokenButtons = document.querySelectorAll(".player-token-button")
 
-let play = document.querySelector(".play-game-button")
-play.addEventListener("click", () => {
+playButton.addEventListener("click", () => {
+  playButton.classList.add("hidden")
+  modeButtons.forEach((element) => {
+    element.classList.remove("hidden")
+  })
 
-  document.getElementById("game").style.display = "block"
-  document.querySelector(".play-game-button").style.display = "none"
+})
 
-  let connectX = new ConnectX(gameConfiguration)
+modeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modeButtons.forEach((button) => {
+      button.classList.add("hidden")
+    })
+    gameConfiguration.tokensPerLine = parseInt(button.dataset.mode)
+    tokenButtons.forEach((button) => {
+      button.classList.remove("hidden")
+    })
+  })
+})
 
-  connectX.createAndDrawGraphicalBoard()
-  connectX.createAnDrawTokens()
-  connectX.activateTokenDropping()
+tokenButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (gameConfiguration.tokenColorPlayer1 === "") {
+      gameConfiguration.tokenColorPlayer1 = button.dataset.token
+      button.classList.add("hidden")
+    }else{
+      gameConfiguration.tokenColorPlayer2 = button.dataset.token
+      tokenButtons.forEach((button) => {
+        button.classList.add("hidden")
+      })
+      console.log(gameConfiguration)
+      new ConnectX(gameConfiguration)
+      document.getElementById("game").classList.remove("hidden")
+    }
+  })
 })
